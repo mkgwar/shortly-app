6d8a97b56a56030eb127d62ac7acab7a6a1d7179
mkgwar <mohitgangwarknp@gmail.com>
2022-01-22 21:17:52 +0530

    hamburger menu added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import "./App.css";
+import { useState, useRef } from "react";
 
 const App = () => {
+  const [showMenu, setshowMenu] = useState(false);
+
   return (
     <div className="bg-gradient-to-b from-white to-gray-100">
-      <Navbar />
-      <Header />
+      {Navbar(showMenu, setshowMenu)}
+      {Header(showMenu)}
       <main className="overflow-auto">
         <Input />
         <Features />
@@ -15,14 +18,17 @@ const App = () => {
   );
 };
 
-const Navbar = () => {
+const Navbar = (showMenu, setshowMenu) => {
   return (
-    <nav className="flex w-full items-center justify-between py-12 px-8">
+    <nav className="flex w-full items-center justify-between p-8">
       <div>
         <img src="/images/logo.svg" alt="logo" />
       </div>
 
-      <div className="h-6 flex flex-col gap-1">
+      <div
+        className="h-6 flex flex-col gap-1 cursor-pointer"
+        onClick={() => setshowMenu(!showMenu)}
+      >
         <div className="bg-Grayish-Violet h-1/3 w-8" />
         <div className="bg-Grayish-Violet h-1/3 w-8" />
         <div className="bg-Grayish-Violet h-1/3 w-8" />
@@ -31,9 +37,29 @@ const Navbar = () => {
   );
 };
 
-const Header = () => {
+const HamburgerMenu = (showMenu) => {
   return (
-    <header className="flex flex-col items-center overflow-auto">
+    <div
+      className={
+        "hamburger flex flex-col justify-center text-center w-11/12 bg-Dark-Violet text-white p-8 font-black absolute top-0 rounded-xl " +
+        (showMenu ? "block" : "hidden")
+      }
+    >
+      <a href="https://google.com">Features</a>
+      <a href="https://google.com">Pricing</a>
+      <a href="https://google.com">Resources</a>
+      <div className="w-full h-0.5 bg-Grayish-Violet mb-8" />
+      <a href="https://google.com">Login</a>
+      <button className=" rounded-full">Sign Up</button>
+    </div>
+  );
+};
+
+const Header = (showMenu) => {
+  return (
+    <header className="flex flex-col items-center overflow-auto relative">
+      {HamburgerMenu(showMenu)}
+
       <div className="h-96 w-full pl-8">
         <img
           src="/images/illustration-working.svg"
@@ -54,6 +80,22 @@ const Header = () => {
 };
 
 const Input = () => {
+  const showWarning = ["border-Red-custom", "visible"];
+  const hideWarning = ["border-transparent", "invisible"];
+
+  const [warning, setwarning] = useState(hideWarning);
+  const inputContent = useRef("");
+
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    if (inputContent.current.value === "") {
+      setwarning(showWarning);
+    } else {
+      setwarning(hideWarning);
+    }
+  };
+
   return (
     <div className="flex flex-col w-11/12 mx-auto p-8 bg-Dark-Violet relative  rounded-xl overflow-hidden mt-20">
       <img
@@ -64,27 +106,35 @@ const Input = () => {
       <input
         type="text"
         placeholder="Shorten a link here..."
-        className="p-4 font-black rounded-lg z-20 focus:outline-0 border-4 border-transparent"
+        ref={inputContent}
+        className={
+          "p-4 font-black rounded-lg z-20 focus:outline-0 border-4 " +
+          warning[0]
+        }
       />
-      <span className="text-Red-custom z-20 text-sm italic mt-2">
+      <span
+        className={"text-Red-custom z-20 text-sm italic mt-2 " + warning[1]}
+      >
         Please add a link
       </span>
-      <button className="rounded-lg z-20 mt-4">Shorten It!</button>
+      <button className="rounded-lg z-20 mt-4" onClick={submitHandler}>
+        Shorten It!
+      </button>
     </div>
   );
 };
 
-const url = ["brand-recognition", "detailed-records", "fully-customizable"];
-
-const title = ["Brand Recognition", "Detailed Records", "Fully Customizable"];
-
-const paragraph = [
-  "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
-  "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
-  "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
-];
-
 const Features = () => {
+  const url = ["brand-recognition", "detailed-records", "fully-customizable"];
+
+  const title = ["Brand Recognition", "Detailed Records", "Fully Customizable"];
+
+  const paragraph = [
+    "Boost your brand recognition with each click. Generic links don’t mean a thing. Branded links help instil confidence in your content.",
+    "Gain insights into who is clicking your links. Knowing when and where people engage with your content helps inform better decisions.",
+    "Improve brand awareness and content discoverability through customizable links, supercharging audience engagement.",
+  ];
+
   return (
     <>
       <div className="w-11/12 mx-auto text-center p-8 mt-16 mb-20">
